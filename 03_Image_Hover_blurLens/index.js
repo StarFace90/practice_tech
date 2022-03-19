9f5d65551da2f13cc6265c581a8433ec2de1e264
woody-park <starface1009@gmail.com>
2022-03-19 23:54:01 +0900

    마우스 움직임 이벤트 주기

--- 03_Image_Hover_blurLens/index.js
+++ 03_Image_Hover_blurLens/index.js
@@ -9,4 +9,11 @@ const circle = document.getElementById("circle");
 layer.addEventListener("mousemove", (event) => {
     circle.style.setProperty("--x", `${100 - event.offsetX}px`);
     circle.style.setProperty("--y", `${100 - event.offsetY}px`);
+
+    var x = event.offsetX;
+    var y = event.offsetY;
+
+    circle.style.left = x + "px";
+    circle.style.top = y + "px";
+
 });
